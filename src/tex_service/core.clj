a04ee94d0d090bb5dc0f60cc9505284d2e1499c5
Thomas Greve Kristensen <tgk@cs.au.dk>
2010-08-30 20:36:33 +0200

    Initial repo

---
+++ src/tex_service/core.clj
@@ -0,0 +1,1 @@
+(ns tex-service.core)
---
+++ test/tex_service/test/core.clj
@@ -0,0 +1,6 @@
+(ns tex-service.test.core
+  (:use [tex-service.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
