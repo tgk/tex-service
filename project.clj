c94c9ffeca7f70062c7c82f353bdea828d437b75
Thomas Greve Kristensen <tgk@cs.au.dk>
2010-08-30 20:42:02 +0200

    Updated project,,clj with correct deps.

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject tex-service "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]])
+(defproject tex-service "0.0.1-SNAPSHOT"
+  :description "A clojure web service for generating LaTeX formulas."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [net.sf.alxa/jlatexmath "0.9.1-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :repositories {"alxa-repo" "http://alxa.sourceforge.net/m2"})
